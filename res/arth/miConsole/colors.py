0ae4144c0258fde58132e936a559ab3f61a2c8d6
Arthur Melgoza <38125347+arthurmv@users.noreply.github.com>
2022-06-30 04:09:22 -0700

    Create colors.py

---
+++ res/arth/miConsole/colors.py
@@ -0,0 +1,23 @@
+from colorama import Fore, Back, Style
+
+consoleCharColor = Fore
+
+consoleCharBlack = Fore.BLACK
+consoleCharBlue = Fore.BLUE
+consoleCharCyan = Fore.CYAN
+consoleCharGreen = Fore.GREEN
+consoleCharMagenta = Fore.MAGENTA
+consoleCharRed = Fore.RED
+consoleCharWhite = Fore.WHITE
+consoleCharYellow = Fore.YELLOW
+
+consoleCharLightBlack = Fore.LIGHTBLACK_EX
+consoleCharLightBlue = Fore.LIGHTBLUE_EX
+consoleCharLightCyan = Fore.LIGHTCYAN_EX
+consoleCharLightGreen = Fore.LIGHTGREEN_EX
+consoleCharLightMagenta = Fore.LIGHTMAGENTA_EX
+consoleCharLightRed = Fore.LIGHTRED_EX
+consoleCharLightWhite = Fore.LIGHTWHITE_EX
+consoleCharLightYellow = Fore.LIGHTYELLOW_EX
+
+consoleCharColorReset = Fore.RESET
